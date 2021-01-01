edfa1396765e295ee07f0d675c21dd15886cb5cf
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-01-01 16:18:36 +0100

    Update guildMemberAdd.js

--- events/member/guildMemberAdd.js
+++ events/member/guildMemberAdd.js
@@ -1,6 +1,11 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = async (client, member, settings) => {
+  let channel = message.guild.channels.cache.get(c => c.name === settings.welcomeChannel);
+  
+  const embed = new MessageEmbed()
+  .setColor('BLACK')
+  .setDescription(`Welcome ${user}`)
 
   await client.createUser({
     guildID: member.guild.id,
