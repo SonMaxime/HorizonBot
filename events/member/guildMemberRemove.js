cdc6ac4169a16c621865957189b998d2f20d4f11
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-01-01 16:18:58 +0100

    Update guildMemberRemove.js

--- events/member/guildMemberRemove.js
+++ events/member/guildMemberRemove.js
@@ -1,3 +1,9 @@
 const { MessageEmbed } = require("discord.js");
 
-module.exports = (client, message, member, settings) => {}
+module.exports = (client, message, member, settings) => {
+   let channel = message.guild.channels.cache.get(c => c.name === settings.welcomeChannel);
+  
+   const embed = new MessageEmbed()
+   .setColor('BLACK')
+   .setDescription(`Welcome ${user}`)
+}
