fecc77625cdfadc296028034a97687e77e183537
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-01-18 20:22:36 +0100

    Update tts.js

--- commands/Admin/tts.js
+++ commands/Admin/tts.js
@@ -1,24 +1,29 @@
 const discordTTS = require('discord-tts');
 
 module.exports.run = async (client, message, args) => {
+    message.delete();
     const argsJoin = args.join(" ");
-    const broadcast = client.voice.createBroadcast();
-    var channelId = message.member.voice.channelID;
-    var channel = client.channels.cache.get(channelId);
-    channel.join().then(connection => {
-    broadcast.play(discordTTS.getVoiceStream(argsJoin));
-    const dispatcher = connection.play(broadcast);
-   });
+    const queue = message.client.queue.get(message.guild.id);
+    if (queue) {
+        queue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id).catch(console.error));
+    } else {
+        const broadcast = client.voice.createBroadcast();
+        var channelId = message.member.voice.channelID;
+        var channel = client.channels.cache.get(channelId);
+        channel.join().then(connection => {
+            broadcast.play(discordTTS.getVoiceStream(argsJoin));
+            const dispatcher = connection.play(broadcast);
+        });
+    }
 }
 
 module.exports.help = {
     name: "tts",
     aliases: ['tts'],
-    category: 'admin',
     description: "test tts",
     cooldown: 3,
-    usage: '<value>',
+    usage: '<args>',
     isUserAdmin: false,
-    permissions: true,
+    permissions: false,
     args: true
 }
