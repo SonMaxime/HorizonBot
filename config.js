a9119af2f9a748ea541f369ea8630f78e4e6288f
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-01-25 08:46:21 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  TOKEN: "NzY1NjQxMTk4NDA0NTAxNTI0.X4Xw7g.iNaIGmkafeHhPRjaaPlXmyB3Iy8",
+  FORTNITE: "68e0bff9-69e0-459e-83d0-f0a11985a8f5",
+  MAX_PLAYLIST_SIZE: 25,
+  PRUNING: true,
+  STAY_TIME: 10,
+  DEFAULT_VOLUME: 100,
+  id: "765641198404501524",
+  clientSecret: "0C2UOX-iw9-3kLZ5Uz8kA2I8pubIprf_",
+  domain: "https://horizonbotsm.herokuapp.com/",
+  port: 3000,
+  SOUNDCLOUD_CLIENT_ID: "HpnNV7hjv2C95uvBE55HuKBUOQGzNDQM",
+  DBCONNECTION: "mongodb+srv://sonmaxime:maxime1905@horizonbot.j5gtv.mongodb.net/horizon?retryWrites=true&w=majority",
+  DEFAULTSETTINGS: {
+    prefix: ".",
+    logChannel: "reports",
+    welcomeMessage: "Bienvenue ! {{user}}",
+    welcomeChannel: "bienvenue",
+    leaveMessage: "Adieu... {{user}}"
+  }
+}
