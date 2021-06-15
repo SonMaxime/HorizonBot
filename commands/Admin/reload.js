f611133fed201fdb12053214807684958f9790f3
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-06-15 14:15:51 +0200

    Delete reload.js

--- commands/Admin/reload.js
+++
@@ -1,9 +0,0 @@
-const { MESSAGES } = require('../../util/constants');
-
-module.exports.run = async (client, message, args, settings) => {
-  await message.delete();
-  await message.channel.send(message.guild.language.restartBot);
-  process.exit();
-};
-
-module.exports.help = MESSAGES.COMMANDS.ADMIN.RELOAD;
